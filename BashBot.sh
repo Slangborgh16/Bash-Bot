69f9d2e1eda34bb2255840f6089bbb48d290642f
Slangborgh16 <slangborgh16@gmail.com>
2017-11-02 16:57:15 -0400

    Added colors! (And other small changes)

--- BashBot.sh
+++ BashBot.sh
@@ -1,98 +1,115 @@
 #!/bin/bash
 
+RED='\033[0;31m' #Alert
+BLUE='\033[0;34m' #Status message
+GREEN='\033[0;32m' #Success
+ORANGE='\033[0;33m' #Input
+NC='\033[0m'
+
 echo " ____            _       ____        _"
 echo "| __ )  __ _ ___| |__   | __ )  ___ | |_"
 echo "|  _ \ / _' / __| '_ \  |  _ \ / _ \| __|"
 echo "| |_) | (_| \__ \ | | | | |_) | (_) | |_"
 echo "|____/ \__,_|___/_| |_| |____/ \___/ \__|"
 echo
-echo "Script written by Samuel Langborgh and Ian McMenamin"
+echo -e "${BLUE}Script written by Samuel Langborgh and Ian McMenamin"
 echo
-echo "Note: This script will not complete the CyberPatriot Ubuntu Box,"
-echo "but it will simplify a lot of the process"
+echo -e "${RED}Note: This script will not complete the CyberPatriot Ubuntu Box,"
+echo -e "but it will simplify a lot of the process${NC}"
 echo
-echo "Press any key to continue"
+echo -e "${ORANGE}Press any key to continue"
 read -n 1 -s
 echo
 
 if [ "$EUID" -eq 0 ]; then
+	echo -e "${BLUE}Checking for and running system updates${NC}"
 	apt-get update
 	apt-get upgrade
 	echo
 
 	#Installs Synaptic
-	echo "Installing Synaptic"
+	echo -e "${BLUE}Installing Synaptic${NC}"
 	apt-get install synaptic
 	echo
 	#Enables and configures firewall
-	echo "Enabling and configuring firewall"
+	echo -e "${BLUE}Enabling and configuring firewall${NC}"
 	ufw enable
 	ufw default deny incoming
 	ufw  default allow incoming
 	echo
 
 	#Enforces password policy
-	echo "Checking if cracklib is installed"
+	echo -e "${BLUE}Checking if cracklib is installed"
 	if [ -n "$( apt-cache policy libpam-cracklib )" ]; then
-		echo "Cracklib installed"
+		echo -e "${GREEN}Cracklib installed"
 	else
-		echo "Cracklib is not installed"
-		echo "Installing cracklib now"
-		echo
+		echo -e "${RED}Cracklib is not installed"
+		echo -e "${BLUE}Installing cracklib now${NC}"
 		apt-get install libpam-cracklib
-		echo "Cracklib installed"
+		echo -e "${GREEN}Cracklib installed"
 		echo
 	fi
-	echo "Password policy to be implemented"
+	echo -e "${RED}Password policy to be implemented"
 	echo
 
 	#Disables guest account
 	guestpath="/etc/lightdm/"
 	guestfile="lightdm.conf"
+	echo -e "${BLUE}Checking if $guestfile exists"
 	if [ -n "$( find $guestpath -name $guestfile )" ]; then
-		echo "File $guestfile exists"
-		echo "Checking to see if guest account is disabled"
-		echo
+		echo -e "${GREEN}File $guestfile exists"
+		echo -e "${BLUE}Checking to see if guest account is disabled"
 		if [ -z $( grep allow-guest=false $guestpath$guestfile ) ]; then
 			if [ -z $( grep allow-guest=true $guestpath$guestfile ) ]; then
-				echo "Guest account status not specified"
-				echo "Disabling guest account"
-				echo -e "allow-guest=false" >> $guestpath$guestfile
-				echo "Guest account disabled"
+				echo -e "${RED}Guest account status not specified"
+				echo -e "${BLUE}Disabling guest account"
+				echo "allow-guest=false" >> $guestpath$guestfile
+				echo -e "${GREEN}Guest account disabled"
 				echo
 			else
-				echo "Guest account enabled"
-				echo "Disabling guest account"
+				echo -e "${RED}Guest account enabled"
+				echo -e "${BLUE}Disabling guest account"
 				sed -i  's/true/false/g' $guestpath$guestfile
-				echo "Guest account disabled"
+				echo -e "${GREEN}Guest account disabled"
 				echo
 			fi
 		else
-			echo "Guest account disabled"
+			echo -e "${GREEN}Guest account disabled"
 			echo
 		fi
 	else
-		echo "File $guestfile does not exist"
-		echo "Creating file now"
+		echo -e "${RED}File $guestfile does not exist"
+		echo -e "${BLUE}Creating $guestfile now"
 		echo -e "[SeatDefaults]\nuser-session=ubuntu\ngreeter-session=unity-greeter\nallow-guest=false" > $guestpath$guestfile
-		echo "$guestfile created and guest account disabled"
+		echo -e "${GREEN}$guestfile created and guest account disabled"
 		echo
 	fi
 
 	#Secures SSH
 	sshpath="/etc/ssh/"
 	sshconfig="sshd_config"
-	echo "Securing SSH"
+	echo -e "${BLUE}Securing SSH"
 	if [ -n "$( find $sshpath -name $sshconfig )" ]; then
-		echo  "File $sshconfig exists"
-		sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' $sshpath$sshconfig
-		echo "SSH root login disabled"
+		echo -e "${GREEN}File $sshconfig exists"
+		echo -e "${BLUE}Checking if SSH root login is disabled"
+		if [ -n "$( grep 'PermitRootLogin yes' $sshpath$sshconfig )" ]; then
+			echo -e "${RED}SSH root login enabled"
+			echo -e "${BLUE}Disabling SSH root login"
+			sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' $sshpath$sshconfig
+			echo -e "${GREEN}SSH root login disabled"
+		else
+			if [ -n "$( grep 'PermitRootLogin no' $sshpath$sshconfig )" ]; then
+				echo -e "${GREEN}SSH root login disabled"
+			else
+				echo -e "${RED}SSH root login preferences not specified"
+			fi
+		fi
 	else
-		echo "File $sshconfig does not exist"
+		echo -e "${RED}File $sshconfig does not exist"
 		echo 
 	fi
 else
-	echo "Please run as root"
+	echo -e "${RED}Please run as root"
 	read -n 1 -s
 	exit
 fi
